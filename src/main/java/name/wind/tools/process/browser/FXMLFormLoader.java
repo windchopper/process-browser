e426dbead277d30950d046a8a57ea19a178a2646
wind <wind@comtv.ru>
2016-07-30 02:33:04 +0300

    no commit message

--- src/main/java/name/wind/tools/process/browser/FXMLFormLoader.java
+++ src/main/java/name/wind/tools/process/browser/FXMLFormLoader.java
@@ -5,8 +5,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import name.wind.common.cdi.BeanReference;
-import name.wind.tools.process.browser.events.FXMLLocationLiteral;
+import name.wind.common.cdi.temporary.TemporaryContext;
 import name.wind.tools.process.browser.events.FXMLFormOpen;
+import name.wind.tools.process.browser.events.FXMLLocationLiteral;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
@@ -19,18 +20,21 @@ import java.io.InputStream;
     @Inject private FXMLLoader fxmlLoader;
 
     protected void formOpen(@Observes FXMLFormOpen fxmlFormOpen) throws IOException {
-        try (InputStream inputStream = fxmlFormOpen.resourceAsStream()) {
-            FXMLLocationLiteral fxmlLocationLiteral = new FXMLLocationLiteral(fxmlFormOpen.resource());
-            BeanReference controllerReference = new BeanReference().withType(StageController.class).withQualifiers(fxmlLocationLiteral);
-            StageController controller = (StageController) controllerReference.resolve();
-            fxmlLoader.setController(controller);
-            Parent sceneRoot = fxmlLoader.load(inputStream);
-            Scene scene = new Scene(sceneRoot);
-            Stage stage = fxmlFormOpen.stage();
-            stage.setScene(scene);
-            controller.start(stage, fxmlFormOpen.resource(), fxmlFormOpen.parameters());
-            stage.show();
-        }
+        TemporaryContext.callWithinTemporaryScope(() -> {
+            try (InputStream inputStream = fxmlFormOpen.resourceAsStream()) {
+                FXMLLocationLiteral fxmlLocationLiteral = new FXMLLocationLiteral(fxmlFormOpen.resource());
+                BeanReference controllerReference = new BeanReference().withType(StageController.class).withQualifiers(fxmlLocationLiteral);
+                StageController controller = (StageController) controllerReference.resolve();
+                fxmlLoader.setController(controller);
+                Parent sceneRoot = fxmlLoader.load(inputStream);
+                Scene scene = new Scene(sceneRoot);
+                Stage stage = fxmlFormOpen.stage();
+                stage.setScene(scene);
+                controller.start(stage, fxmlFormOpen.resource(), fxmlFormOpen.parameters());
+                stage.show();
+                return null;
+            }
+        });
     }
 
 }
--- src/main/java/name/wind/tools/process/browser/FXMLLoaderProducer.java
+++ src/main/java/name/wind/tools/process/browser/FXMLLoaderProducer.java
@@ -2,6 +2,7 @@ package name.wind.tools.process.browser;
 
 import javafx.fxml.FXMLLoader;
 import name.wind.common.cdi.AbstractProducer;
+import name.wind.common.cdi.temporary.TemporaryScoped;
 import name.wind.common.util.Builder;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -9,7 +10,7 @@ import javax.enterprise.inject.Produces;
 
 @ApplicationScoped public class FXMLLoaderProducer extends AbstractProducer implements ResourceBundleAware {
 
-    @Produces protected FXMLLoader createFXMLLoader() {
+    @Produces @TemporaryScoped protected FXMLLoader createFXMLLoader() {
         return Builder.direct(FXMLLoader::new)
             .set(target -> target::setResources, bundle)
             .get();
