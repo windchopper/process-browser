472fec62d70044e335cf25f206a7fd77509288aa
wind <wind@comtv.ru>
2016-11-28 02:00:06 +0300

    no commit message

--- src/main/java/name/wind/tools/process/browser/PreferencesAware.java
+++ src/main/java/name/wind/tools/process/browser/PreferencesAware.java
@@ -1,31 +1,24 @@
 package name.wind.tools.process.browser;
 
-import name.wind.common.preferences.DefaultPreferencesEntry;
 import name.wind.common.preferences.PreferencesEntry;
-import name.wind.common.preferences.PreferencesEntryType;
-import name.wind.common.preferences.store.DefaultFlatPreferencesStorage;
-import name.wind.common.preferences.store.PreferencesStorage;
+import name.wind.common.preferences.types.FlatType;
 
 import java.io.File;
 import java.time.Duration;
+import java.util.prefs.Preferences;
 
 public interface PreferencesAware {
 
-    PreferencesStorage<String> preferencesStorage = new DefaultFlatPreferencesStorage("/name/wind/tools/process/browser");
+    Preferences preferencesNode = Preferences.userRoot().node("/name/wind/tools/process/browser");
+    Duration defaultBufferLifetime = Duration.ofMinutes(1);
 
     String PREFERENCES_ENTRY_NAME__FILTER_TEXT = "filterText";
     String PREFERENCES_ENTRY_NAME__BROWSE_INITIAL_DIRECTORY = "browseInitialDirectory";
 
-    PreferencesEntry<String> filterTextPreferencesEntry = new DefaultPreferencesEntry<>(
-        Duration.ofMinutes(1),
-        preferencesStorage,
-        PREFERENCES_ENTRY_NAME__FILTER_TEXT,
-        PreferencesEntryType.stringType);
+    PreferencesEntry<String> filterTextPreferencesEntry = new PreferencesEntry<>(
+        preferencesNode, PREFERENCES_ENTRY_NAME__FILTER_TEXT, new FlatType<>(string -> string, string -> string), defaultBufferLifetime);
 
-    PreferencesEntry<File> browseInitialDirectoryPreferencesEntry = new DefaultPreferencesEntry<>(
-        Duration.ofMinutes(1),
-        preferencesStorage,
-        PREFERENCES_ENTRY_NAME__BROWSE_INITIAL_DIRECTORY,
-        PreferencesEntryType.fileType);
+    PreferencesEntry<File> browseInitialDirectoryPreferencesEntry = new PreferencesEntry<>(
+        preferencesNode, PREFERENCES_ENTRY_NAME__BROWSE_INITIAL_DIRECTORY, new FlatType<>(File::new, File::getAbsolutePath), defaultBufferLifetime);
 
 }
