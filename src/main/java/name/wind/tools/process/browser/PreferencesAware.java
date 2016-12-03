7fa8ecef26359221ebc07bb63ac655cf2904de6c
wind <wind@comtv.ru>
2016-12-03 21:15:59 +0300

    no commit message

--- src/main/java/name/wind/tools/process/browser/PreferencesAware.java
+++ src/main/java/name/wind/tools/process/browser/PreferencesAware.java
@@ -1,6 +1,8 @@
 package name.wind.tools.process.browser;
 
+import name.wind.common.preferences.PlatformPreferencesStorage;
 import name.wind.common.preferences.PreferencesEntry;
+import name.wind.common.preferences.PreferencesStorage;
 import name.wind.common.preferences.types.FlatType;
 
 import java.io.File;
@@ -9,16 +11,17 @@ import java.util.prefs.Preferences;
 
 public interface PreferencesAware {
 
-    Preferences preferencesNode = Preferences.userRoot().node("/name/wind/tools/process/browser");
     Duration defaultBufferLifetime = Duration.ofMinutes(1);
+    PreferencesStorage preferencesStorage = new PlatformPreferencesStorage(
+        Preferences.userRoot().node("name/wind/tools/process/browser"));
 
     String PREFERENCES_ENTRY_NAME__FILTER_TEXT = "filterText";
     String PREFERENCES_ENTRY_NAME__BROWSE_INITIAL_DIRECTORY = "browseInitialDirectory";
 
     PreferencesEntry<String> filterTextPreferencesEntry = new PreferencesEntry<>(
-        preferencesNode, PREFERENCES_ENTRY_NAME__FILTER_TEXT, new FlatType<>(string -> string, string -> string), defaultBufferLifetime);
+        preferencesStorage, PREFERENCES_ENTRY_NAME__FILTER_TEXT, new FlatType<>(string -> string, string -> string), defaultBufferLifetime);
 
     PreferencesEntry<File> browseInitialDirectoryPreferencesEntry = new PreferencesEntry<>(
-        preferencesNode, PREFERENCES_ENTRY_NAME__BROWSE_INITIAL_DIRECTORY, new FlatType<>(File::new, File::getAbsolutePath), defaultBufferLifetime);
+        preferencesStorage, PREFERENCES_ENTRY_NAME__BROWSE_INITIAL_DIRECTORY, new FlatType<>(File::new, File::getAbsolutePath), defaultBufferLifetime);
 
 }
