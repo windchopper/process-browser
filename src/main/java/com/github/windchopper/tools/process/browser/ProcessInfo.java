b46d2b1deda2ac1138bdcd2308a8b2724f42a0d3
wind <wind@comtv.ru>
2020-05-11 03:38:36 +0300

    no commit message

--- src/main/java/com/github/windchopper/tools/process/browser/ProcessInfo.java
+++ src/main/java/com/github/windchopper/tools/process/browser/ProcessInfo.java
@@ -2,6 +2,7 @@ package com.github.windchopper.tools.process.browser;
 
 import com.github.windchopper.common.fx.CellFactories;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
@@ -9,7 +10,7 @@ import javafx.util.Callback;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Optional;
+import java.util.Objects;
 
 public class ProcessInfo {
 
@@ -17,6 +18,11 @@ public class ProcessInfo {
     private final long parentPid;
     private final String name;
     private final String command;
+
+    private final StringProperty pidProperty;
+    private final StringProperty parentPidProperty;
+    private final StringProperty nameProperty;
+    private final StringProperty commandProperty;
 
     public ProcessInfo(ProcessHandle processHandle) {
         pid = processHandle.pid();
@@ -26,17 +32,22 @@ public class ProcessInfo {
         name = processHandle.info().command()
             .map(Paths::get)
             .map(Path::getFileName)
-            .map(Object::toString)
+            .map(Objects::toString)
             .orElse("?");
         command = processHandle.info().command()
             .orElse("?");
+
+        pidProperty = new ReadOnlyStringWrapper(Objects.toString(pid));
+        parentPidProperty = new ReadOnlyStringWrapper(parentPid < 0 ? "" : Objects.toString(parentPid));
+        nameProperty = new ReadOnlyStringWrapper(name);
+        commandProperty = new ReadOnlyStringWrapper(command);
     }
 
     public long pid() {
         return pid;
     }
 
-    public long parendPid() {
+    public long parentPid() {
         return parentPid;
     }
 
@@ -58,23 +69,12 @@ public class ProcessInfo {
     }
 
     public static Callback<TableColumn.CellDataFeatures<ProcessInfo, String>, ObservableValue<String>> tableCellValueFactory() {
-        return (features) -> switch (features.getTableColumn().getId()) {
+        return (features) -> features.getValue() == null ? null : switch (features.getTableColumn().getId()) {
             default -> null;
-            case "identifierColumn" -> new ReadOnlyStringWrapper(Optional.ofNullable(features.getValue())
-                .map(ProcessInfo::pid)
-                .map(Object::toString)
-                .orElse(null));
-            case "parentIdentifierColumn" -> new ReadOnlyStringWrapper(Optional.ofNullable(features.getValue())
-                .map(ProcessInfo::parendPid)
-                .filter(pid -> pid >= 0)
-                .map(Object::toString)
-                .orElse(null));
-            case "nameColumn" -> new ReadOnlyStringWrapper(Optional.ofNullable(features.getValue())
-                .map(ProcessInfo::name)
-                .orElse(null));
-            case "executablePathColumn" -> new ReadOnlyStringWrapper(Optional.ofNullable(features.getValue())
-                .map(ProcessInfo::command)
-                .orElse(null));
+            case "identifierColumn" -> features.getValue().pidProperty;
+            case "parentIdentifierColumn" -> features.getValue().parentPidProperty;
+            case "nameColumn" -> features.getValue().nameProperty;
+            case "executablePathColumn" -> features.getValue().commandProperty;
         };
     }
 
